f9debaf345489c90df72d0fdd936d4b456b5f066
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-03-10 17:58:12 -0500

    Creo middleware para validar la informacion del usuario.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,15 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const router = Router();
 
 const { create } = require('../controllers/auth');
+const { validateParams } = require('../middleware/validate.params');
 
-router.post('/signup', create);
+router.post('/signup', [
+    check('name', "El nombre es obligatorio").notEmpty(),
+    check('email', "Verifique su email").normalizeEmail().isEmail(),
+    check('password', "Debe ingresar una contraseña").exists(),
+    validateParams
+], create);
 
 module.exports = router;
