37d9c2e5346812a4c50b977d4972a220e3945b36
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-03-10 18:39:00 -0500

    Creo esquema para los usuarios. Ahora se pueden almacenar usuarios en Mongodb.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,36 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
 
-const create = (req, res = response) => {
-    res.json({
-        ok: true,
-        message: 'Registrar usuario'
-    });
+const User = require('../models/user.model');
+
+const create = async (req, res = response) => {
+    const { email, password } = req.body;
+    try {
+
+        let existeEmail = await User.findOne({ email });
+        if (existeEmail)
+            return res.status(400).json({
+                ok: false,
+                message: "Ese email ya esta registrado."
+            });
+
+        const user = new User(req.body);
+
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(password, salt);
+
+        await user.save();
+        res.json({
+            ok: true,
+            user
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            message: "Contacte al administrador."
+        });
+    }
 }
 
 module.exports = { create }
